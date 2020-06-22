b965a62c9f599de1d5185f3d102993536d0c0cba
Dipu Raj <dipumedayil@gmail.com>
2020-06-22 22:43:41 +0530

    React smart Tab v2

---
+++ projects/demo/react-smart-tab/v2/precache-manifest.da9677cf75ea394bca8efdf4cd4a9f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0196f10eaed99f202b700ce3d7cce85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cdb9dd8857ba9786c7a",
+    "url": "./static/css/main.5c2360a7.chunk.css"
+  },
+  {
+    "revision": "31991c4f70d91af1da51",
+    "url": "./static/js/2.a982c2a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a982c2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdb9dd8857ba9786c7a",
+    "url": "./static/js/main.d7a9026a.chunk.js"
+  },
+  {
+    "revision": "25e8064fabae89f32d33",
+    "url": "./static/js/runtime-main.3e63bef5.js"
+  }
+]);
